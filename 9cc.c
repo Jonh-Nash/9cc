470fae61f6cccf4a691771572c21a04bddad3873
justy <vicious8424687@gmail.com>
2025-11-04 14:52:19 +0000

    update error message

--- 9cc.c
+++ 9cc.c
@@ -21,6 +21,9 @@ struct Token {
   char *str;      // トークン文字列
 };
 
+// 入力プログラム
+char *user_input;
+
 // 現在着目しているトークン
 Token *token;
 
@@ -34,6 +37,19 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて真を返す。それ以外の場合には偽を返す。
 bool consume(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
@@ -45,14 +61,14 @@ bool consume(char op) {
 // 次のトークンが期待している記号のときには、トークンを1つ読み進める。それ以外の場合にはエラーを報告する。
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
 
 // 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。それ以外の場合にはエラーを報告する。
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -95,7 +111,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(p, "トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -109,7 +125,8 @@ int main(int argc, char **argv) {
   }
 
   // トークナイズする
-  token = tokenize(argv[1]);
+  user_input = argv[1];
+  token = tokenize(user_input);
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
